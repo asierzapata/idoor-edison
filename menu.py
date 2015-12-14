05e4b740eaf8ab893c8d32eb0e6723f679fe69e1
xogne <asier.zapata@gmail.com>
2015-12-14 17:52:06 +0100

    AddFriend and time out update

--- menu.py
+++ menu.py
@@ -2,12 +2,6 @@
 # encoding: utf-8
 
 #TODO 
-#     Pagina de avisos (4)
-#     Pagina acceso rapido ( siguiente classe)
-#     Horari de 8 a 20
-#     Crear pagina y botones para cada asignatura 
-#     Colores?	
-#     Uso: Con las teclas tab, down arrow y enter
 
 import npyscreen, random
 #npyscreen.disableColor()
@@ -17,38 +11,66 @@ class TestApp(npyscreen.NPSApp):
 	    parser.groupCreation();
 	    self.controller = controller
     def h_exit_escape(self):
-	    F.on_ok	
+	    F.on_ok
+    def while_waiting():
+    	self.on_ok()
     def main(self):
+    	#Definimos el timeout como 4 segundos
+    	self.keypress_timeout = 400
 	# Creacion del Form y de los botones de la 1a pagina
         F = npyscreen.FormMultiPageActionWithMenus(name = "IDOOR",lines=30,columns=40,pages_label_color='LABEL')
         parser = self.parser
 	#F.how_exited_handers[npyscreen.widget.EXITED_ESCAPE]  = self.exit_application
+	#Creacion de la pagina rapida o 0
         F.add(CustomFixedText, name = "Siguiente Clase: ", value = "Siguiente Clase: " + parser.nextClass())
         F.add(CustomFixedText, name = "Ultima nota: ", value = "Ultima nota: " + parser.lastGrade())
         F.add(CustomFixedText, name = "Ultima tarea: ", value = "Ultima tarea: " + parser.lastAssignment())
         F.add(CSButton, name = "Cerrar session")
+        #Creaciopn de la pagina 1
 	new_page = F.add_page()
 	fn = F.add(NotasButton, name = "Notas")
 	av = F.add(AvisosButton, name = "Avisos")
         dt = F.add(HorarioButton, name = "Horario")
+        fb = F.add(FriendButton, name = "Añadir amigo")
 	lo = F.add(CSButton, name = "Cerrar session")        
-        # Creacion pagina 3
+        # Creacion pagina 2
         new_page_2 = F.add_page()
         t = F.add(CustomTitleText, name = "Notes:",)
         # TODO Cambiar formato del display de las notas a grid
         for x in range(parser.numGrades()):
                 F.add_widget_intelligent(CustomFixedText, value = parser.grades[x][0] + ' ' + parser.grades[x][1] + ' ' + parser.grades[x][2] )
         F.add(BackButton, name = "Volver al menu principal")
-        # Creacion pagina 4
+        # Creacion pagina 3
     	new_page_3 = F.add_page()
     	parser.gridScheduleCreation(F.add(MyGrid, columns = 6, scroll_exit=True, exit_left = True,col_titles=['','Lunes','Martes','Miercoles','Jueves','Viernes']))
         #F.add(BackButton, name = "Volver al menu principal")
-	# Creacion pagina 5
+	# Creacion pagina 3
 	new_page_4 = F.add_page()
 	for x in range(parser.numAssignment()):
 		F.add_widget_intelligent(CustomFixedText, value = parser.assignments[x][0]+':'+ parser.assignments[x][1] +' para ' + parser.assignments[x][2])
         F.add(BackButton, name = "Volver al menu principal")
+        # Creacion pagina 5
+        new_page_5 = F.add_page()
+        F.add(CustomTitleText, name = "Añadir amigo")
+        F.add(CustomFixedText, value = "Acerque la tarjeta de su amigo")
+        check = friendAdd(parser.id)
+        if check is 0:
+            	F.switch_page(6)
+        elif:
+        	F.switch_page(7)
+        F.add(BackButton, name = "Volver al menu principal")
+        # Creacion pagina 6
+        new_page_6 = F.add_page()
+        F.add(CustomFixedText, value = "La tarjeta que acercaste era la tuya. Por favor, acerca la de tu amigo")
+        time.sleep(4)
+        F.switch_page(5)
+        # Creacion pagina 7
+        new_page_7 = F.add_page()
+        F.add(CustomFixedText, value = "Perfecto! Ahora ya soy amigos")
+        F.add(BackButton, name = "Volver al menu principal"
         # Metodo que se llama al seleccionar OK
+        def on_ok():
+            	self.controller.stop()
 	def on_cancel():
 		F.switch_page(0)
 	F.on_cancel = on_cancel
@@ -69,15 +91,15 @@ class MyGrid(npyscreen.GridColTitles):
     scroll_exit = True
     # Modificacion de los colores con los que se hace display el grid
     def custom_print_cell(self, actual_cell, cell_display_value):
-    	if cell_display_value < 5:
+    	if cell_display_value < 5 or cell_display_value is '---':
       		actual_cell.color = 'DANGER'
     	elif cell_display_value >= 5:
        		actual_cell.color = 'GOOD'
        	else:
       		actual_cell.color = 'DEFAULT'
 class BackButton(npyscreen.ButtonPress):
-    def whenPressed(self):
-        self.parent.switch_page(1)
+    	def whenPressed(self):
+        	self.parent.switch_page(1)
         
 class AvisosButton(npyscreen.ButtonPress):
 	def whenPressed(self):
@@ -95,7 +117,10 @@ class HorarioButton(npyscreen.ButtonPress):
 	def whenPressed(self):
 		self.parent.switch_page(3)
 
-	
+class FriendButton(npyscreen.ButtonPress):
+    	def whenPressed(self):
+        	self.parent.switch_page(5)
+        
 if __name__ == "__main__":
     App = TestApp()
     App.run()
